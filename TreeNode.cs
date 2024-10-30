4f9d2d4ad8c0dfdcb394b07c9c6997b4216286da
veratrofimova <45028963+veratrofimova@users.noreply.github.com>
2024-10-31 00:41:09 +0300

    Add files via upload

---
+++ TreeNode.cs
@@ -0,0 +1,14 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace DZ_Lesson_22_TreesAndHeap
+{
+    public class Node<T, F>
+    {
+        public T Value { get; set; }
+        public F Data { get; set; }
+        public Node<T, F> Left { get; set; }
+
+        public Node<T, F> Right { get; set; }
+
+    }
+}
